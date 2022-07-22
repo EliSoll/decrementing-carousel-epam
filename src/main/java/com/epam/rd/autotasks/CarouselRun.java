b0c4ea390f7b69b428a9a90f75de56ad2dbd81d5
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2022-07-22 13:08:34 +0300

    test 1

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -1,13 +1,31 @@
 package com.epam.rd.autotasks;
 
+import static com.epam.rd.autotasks.DecrementingCarousel.array;
+
 public class CarouselRun {
+    Object carousel;
+    int position = 0;
+
+    public CarouselRun(Object carousel) {
+        this.carousel = carousel;
+    }
 
     public int next() {
-       throw new UnsupportedOperationException();
+        if (isFinished())
+            return -1;
+        else {
+            while (array[position %= array.length] <= 0) {
+                position++;
+            }
+        }
+        return array[position++]--;
     }
 
     public boolean isFinished() {
-        throw new UnsupportedOperationException();
+        for (int var: array) {
+            if (var > 0) return false;
+        }
+            return true;
+        }
     }
 
-}
